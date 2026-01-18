96d6df4f024e243e6527f27b43769a2f73081e29
Motoki KAMIMURA <19676305+usabarashi@users.noreply.github.com>
2026-01-18 14:52:01 +0900

    Add Alloy6 (線形時相論理 + 有界モデル検査) でモデリングしてみた (#1)
    * Add Alloy modeling slides and setup Quarto development environment
    
    * Fix link to Alloy modeling slides in README
    
    * Update flake.nix description for Quarto slide environment

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Quarto slide development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      flake-parts,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "aarch64-darwin"
+      ];
+
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              quarto
+            ];
+          };
+        };
+    };
+}
